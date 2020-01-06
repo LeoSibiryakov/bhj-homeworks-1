3f16494b2027eb13c1ab67564e39a0dab706ba96
Leonid Sibiryakov <leo-sibiryakov@yandex.ru>
2020-01-06 18:19:33 +0500

    second commit

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,20 +1,17 @@
-"use strict"
-
-let main = document.getElementById('modal_main');
-let success = document.getElementById('modal_success');
-let close = document.getElementsByClassName('modal_close');
-let show = document.getElementsByClassName('show-success');
+let show = document.querySelector('.show-success');
+let main = document.querySelector('#modal_main');
+let success = document.querySelector('#modal_success');
+let close = document.querySelectorAll('.modal__close');
 
 main.classList.add('modal_active');
 
-for (let i = 0;i < close.length; i++) {
-    close[i].onclick = function() {
-        this.parentElement.parentElement.classList.remove('modal_active');
-    }
+for (let i = 0; close.length > i; i++) {
+    close[i].addEventListener('click', function() {
+        main.classList.remove('modal_active');
+        success.classList.remove('modal_active');
+    })
 }
-for (let i = 0; i < show.length; i++){ 
-  show[i].onclick = function (){
+
+show.addEventListener('click', function() {
     success.classList.add('modal_active');
-    main.remove('modal_active');
-    }
-}
+})
